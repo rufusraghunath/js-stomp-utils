fd2d158680c2faed51bbd86bfa0864fd4fb55e57
Rufus Raghunath <rraghun@thoughtworks.com>
2019-10-20 10:44:08 -0500

    Add missing methods to index.d.ts

--- packages/mock-stomp-broker/src/broker/index.d.ts
+++ packages/mock-stomp-broker/src/broker/index.d.ts
@@ -7,9 +7,11 @@ export = MockStompBroker;
 declare class MockStompBroker {
   constructor(port?: number);
 
+  getPort(): number;
   newSessionsConnected(): Promise<string[]>;
   subscribed(sessionId: string): Promise<void>;
+  sendMessageWithPayloadToTopic(topic: string, payload: any): string;
   messageSent(messageId: string): Promise<void>;
   disconnected(sessionId: string): Promise<void>;
-  sendMessageWithPayloadToTopic(topic: string, payload: any): string;
+  kill(): void;
 }
