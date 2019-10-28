fbc74e7c139411dc94dc9a87b71c87b328e7ae64
Rufus Raghunath <rraghun@thoughtworks.com>
2019-10-27 19:29:20 -0500

    Add proper index.d.ts

--- packages/react-stomp-client/src/client/index.d.ts
+++ packages/react-stomp-client/src/client/index.d.ts
@@ -1,37 +1,33 @@
-// TODO
+declare module "react-stomp-client" {
+  import { Client, Message } from "@stomp/stompjs";
+  import { Component, ReactNode } from "react";
 
-// declare module "mock-stomp-broker" {
-//   interface Config {
-//     port?: number;
-//     portRange?: [number, number];
-//     endpoint?: string;
-//   }
+  interface Props {
+    children: ReactNode;
+    topic?: string;
+    onMessage?: (message: Message) => void;
+    port?: number;
+    debugMode?: boolean;
+  }
 
-//   class MockStompBroker {
-//     private static PORTS_IN_USE;
-//     private static BASE_SESSION;
-//     private static getRandomInt;
-//     private static getPort;
-//     private readonly port;
-//     private readonly httpServer;
-//     private readonly stompServer;
-//     private readonly sentMessageIds;
-//     private queriedSessionIds;
-//     private sessions;
-//     private thereAreNewSessions;
-//     private setMiddleware;
-//     private registerMiddlewares;
+  interface State {
+    stompClient: Client;
+  }
 
-//     constructor({ port, portRange, endpoint }?: Config);
-
-//     public newSessionsConnected(): Promise<string[]>;
-//     public subscribed(sessionId: string): Promise<void>;
-//     public scheduleMessage(topic: string, payload: any, headers?: {}): string;
-//     public messageSent(messageId: string): Promise<void>;
-//     public disconnected(sessionId: string): Promise<void>;
-//     public kill(): void;
-//     public getPort(): number;
-//   }
-
-//   export default MockStompBroker;
-// }
+  export default class StompClient extends Component<Props, State> {
+    static readonly defaultProps: {
+      debugMode: boolean;
+    };
+    private static readonly RECONNECT_DELAY;
+    private static readonly HEARBEAT_FREQUENCY;
+    constructor(props: Props);
+    componentDidMount(): void;
+    componentDidUpdate(prevProps: Props): void;
+    componentWillUnmount(): void;
+    render(): ReactNode;
+    private activateAndSubscribe;
+    private handleStompMessage;
+    private getBrokerUrl;
+    private getNewClient;
+  }
+}
